c82c293b2242a94e4b5a3e1dbefbfd461bec6b81
klozz <carlosj@klozz.dev>
2025-01-11 17:37:58 -0600

    fix(FEAS): Change type of connectedAPI in FEAS_BUFFER_PACKAGE to int32_t
    to resolve narrowing conversion warning.
    
    Change-Id: I806e52978d8d77e4b3b2437fc49f714563485baf
    Signed-off-by: klozz <carlosj@klozz.dev>

--- libs/gui/Surface.cpp
+++ libs/gui/Surface.cpp
@@ -126,10 +126,10 @@ void Surface::ProducerDeathListenerProxy::binderDied(const wp<IBinder>&) {
 namespace {
 
     typedef struct _FEAS_BUFFER_PACKAGE {
-        __u32 pid;
+        uint64_t pid;
         union {
             __u32 start;
-            __u32 connectedAPI;
+            int32_t connectedAPI;
         };
         union {
             __u64 frame_time;
@@ -158,6 +158,11 @@ namespace {
         if (!mFEASDeviceOpen) {
             mFEASDevFd = open(FEAS_IOCTL_PATH, O_RDONLY);
             mFEASDeviceOpen = (mFEASDevFd >= 0);
+            if (mFEASDeviceOpen) {
+                ALOGD("FEAS", "FEAS device opened successfully");
+            } else {
+                ALOGD("FEAS", "Failed to open FEAS device at %s: %s", FEAS_IOCTL_PATH, strerror(errno));
+            }
         }
         return mFEASDeviceOpen;
     }
@@ -252,8 +257,9 @@ namespace {
 
     void feasConnect(const int32_t& api, const uint64_t& identifier) {
         FEAS_BUFFER_PACKAGE msg = {
-            .pid = getpid(),
+            .pid = static_cast<__u32>(getpid()),
             .connectedAPI = api,
+            .frame_id = 0, // Initialize frame_id
             .identifier = identifier
         };
         feasOperation(msg, FEAS_CONNECT);
@@ -261,7 +267,7 @@ namespace {
 
     void feasQueueBEG(const uint64_t& identifier) {
         FEAS_BUFFER_PACKAGE msg = {
-            .pid = getpid(),
+            .pid = static_cast<__u32>(getpid()),
             .start = 1,
             .identifier = identifier
         };
@@ -270,7 +276,7 @@ namespace {
 
     void feasDisconnect(const uint64_t& identifier) {
         FEAS_BUFFER_PACKAGE msg = {
-            .pid = getpid(),
+            .pid = static_cast<__u32>(getpid()),
             .identifier = identifier,
             .connectedAPI = 0
         };
